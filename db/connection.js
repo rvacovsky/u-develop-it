fee4e1c8bb445d8bd7b7aa832304fc24885c16ba
Roger Vacovsky <rvacovsky@gmail.com>
2022-05-21 15:29:33 -0400

    updated MySQL pw

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: 'Margot091015!!!',
+  password: 'membermember123!',
   database: 'election'
 });
 
